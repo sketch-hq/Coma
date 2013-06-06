0496106a32d39239e4db1b7c7698b4c50256bde7
Sam Deane <sam@elegantchaos.com>
2013-06-06 12:43:40 +0100

    Added some initial examples.

---
+++ Extras/Documentation/example.h
@@ -0,0 +1,11 @@
+// Generated .h
+
+@class MyClass;
+
+@interface ExampleClass : BaseClass
+
+@property (strong, nonatomic) NSString* exampleString;
+@property (assign, nonatomic) NSInteger exampleNumber;
+@property (strong, nonatomic) MyClass* exampleCustom;
+
+@end
