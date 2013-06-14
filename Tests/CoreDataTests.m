312714adc1a1428592b0dd7e4bebac1017189803
Sam Deane <sam@elegantchaos.com>
2013-06-14 17:08:55 +0100

    Fixed test of mogenerator core data classes.

--- Tests/CoreDataTests.m
+++ Tests/CoreDataTests.m
@@ -25,6 +25,9 @@ ECDeclareDebugChannel(ComaModelChannel);
 
 @interface CoreDataTests()
 @property (strong, nonatomic) NSDate* date;
+@property (strong, nonatomic) NSManagedObjectContext* context;
+@property (strong, nonatomic) NSManagedObjectModel* model;
+@property (strong, nonatomic) NSPersistentStoreCoordinator* coordinator;
 @end
 
 @implementation CoreDataTests
@@ -37,10 +40,30 @@ ECDeclareDebugChannel(ComaModelChannel);
     ECEnableChannel(ComaTemplatesChannel);
 }
 
+- (void)setupCoreData
+{
+    // create a new model and coordinator
+    NSBundle* mainBundle = [NSBundle mainBundle];
+    NSBundle* thisBundle = [NSBundle bundleForClass:[self class]];
+    NSArray* bundles = (mainBundle == thisBundle) ? @[thisBundle] : @[thisBundle, mainBundle];
+    self.model = [NSManagedObjectModel mergedModelFromBundles:bundles];
+    NSPersistentStoreCoordinator* coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: self.model];
+    [coordinator addPersistentStoreWithType:NSInMemoryStoreType configuration:nil URL:nil options:nil error:nil];
+    NSManagedObjectContext* context = [[NSManagedObjectContext alloc] init];
+    context.persistentStoreCoordinator = coordinator;
+    context.undoManager = nil;
+
+    self.context = context;
+    self.coordinator = coordinator;
+    
+}
+
 - (void)testCoreDataClasses
 {
-    Person* person = [[Person alloc] init];
-    Job* job = [[Job alloc] init];
+    [self setupCoreData];
+
+    Person* person = [[Person alloc] initWithEntity:[NSEntityDescription entityForName:@"Person" inManagedObjectContext:self.context] insertIntoManagedObjectContext:self.context];
+    Job* job = [[Job alloc] initWithEntity:[NSEntityDescription entityForName:@"Job" inManagedObjectContext:self.context] insertIntoManagedObjectContext:self.context];
 
     person.job = job;
 
