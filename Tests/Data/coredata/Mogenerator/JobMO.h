b5a60a02643aa19e28b2c6e1e05583adcfc91410
Sam Deane <sam@elegantchaos.com>
2013-06-14 17:45:21 +0100

    Work in progress on comparison tests between mogenerator and coma output for simple core data model.

--- Tests/Data/coredata/Mogenerator/JobMO.h
+++
@@ -1,5 +0,0 @@
-#import "_Job.h"
-
-@interface Job : _Job {}
-// Custom logic goes here.
-@end
--- Tests/Data/coredata/Mogenerator/PersonMO.h
+++
@@ -1,5 +0,0 @@
-#import "_Person.h"
-
-@interface Person : _Person {}
-// Custom logic goes here.
-@end
