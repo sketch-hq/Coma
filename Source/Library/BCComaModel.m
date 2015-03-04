da9636d65986d13b7bc96e0302ad689e0736b1ba
Sam Deane <sam@elegantchaos.com>
2015-03-04 16:41:42 +0000

    store a copy of the class info under the key "classInfo", so that it can always be accessed explicitly

--- Source/Library/BCComaModel.m
+++ Source/Library/BCComaModel.m
@@ -41,7 +41,7 @@ ECDefineDebugChannel(ComaModelChannel);
     NSMutableDictionary* modelDictionary = [self loadDictionaryAtURL:url];
     [self setupWithModelDictionary:modelDictionary];
   }
-
+  
   return self;
 }
 
@@ -52,26 +52,26 @@ ECDefineDebugChannel(ComaModelChannel);
     self.templates = templates;
     [self setupWithModelDictionary:modelDictionary];
   }
-
+  
   return self;
 }
 
 /**
  Set things up.
-
+ 
  We load the types from the dictionary, processing any inheritance.
  We then preprocess the classes to tidy them up and add some dynamically generated keys.
-
+ 
  */
 
 - (void)setupWithModelDictionary:(NSMutableDictionary*)modelDictionary
 {
   ECDebug(ComaModelChannel, @"setting up");
-
+  
   self.data = modelDictionary;
   self.types = [self loadItemsWithInheritance:modelDictionary[@"types"]];
   self.defaultTypeInfo = self.types[@"«default»"];
-
+  
   [self preprocessTypes];
   [self preprocessClasses];
 }
@@ -95,15 +95,15 @@ ECDefineDebugChannel(ComaModelChannel);
       [ECErrorReporter reportError:error message:@"error loading dictionary from %@", url];
     }
   }
-
+  
   return result;
 }
 
 /**
  Return some items from a dictionary.
-
+ 
  The items are expected to be under a key called "items".
-
+ 
  If a "base" key is present, this is treated as the name of another JSON file to inherit values from.
  The contents of this file are loaded and this routine is called recursively to get items from it.
  These inherited items are then merged in to the original dictionary.
@@ -125,7 +125,7 @@ ECDefineDebugChannel(ComaModelChannel);
       items = baseItems;
     }
   }
-
+  
   return items;
 }
 
@@ -176,7 +176,7 @@ ECDefineDebugChannel(ComaModelChannel);
         resolvedTypeName = alias;
       }
     }
-
+    
     if (![typeName isEqualToString:@"«default»"])
     {
       typeInfo[@"originalTypeName"] = typeName;
@@ -187,17 +187,17 @@ ECDefineDebugChannel(ComaModelChannel);
 
 /**
  Perform some processing on the classes.
-
+ 
  When loaded from the json file, the classes are a dictionary, with the key being the name of the class, and
  the value being another dictionary describing the class.
-
+ 
  This is a cleaner format from the point of view of authoring the file, but at the top level we really want a list instead.
-
+ 
  So we run through each entry in the top level dictionary. Since the top level key for each class is the only thing that gives its
  name, we add these to the dictionary for the class so that we can use it later.
-
+ 
  Whilst we're at it, we run through the attributes and relationships dictionaries in the class and process that a bit too, turning it into a list.
-
+ 
  We then turn the top level dictionary into a list.
  */
 
@@ -208,16 +208,17 @@ ECDefineDebugChannel(ComaModelChannel);
                              @"version" : @"1.0",
                              @"date": [NSDate date]
                              };
-
+  
   NSDictionary* defaults = self.data[@"defaults"];
   NSMutableDictionary* classes = self.data[@"classes"];
   self.classes = classes;
-
+  
   // initial pass through the classes to update some basic info
   // need to do this first so that the typeInfo for all classes have been processed before we start working on the attributes and relationships
   [classes enumerateKeysAndObjectsUsingBlock:^(id className, NSMutableDictionary* info, BOOL *stop) {
     info[@"name"] = className;
     info[@"coma"] = comaInfo;
+    
     // merge in any default values that are missing
     [defaults enumerateKeysAndObjectsUsingBlock:^(id key, id value, BOOL *stop) {
       if (![info objectForKey:key])
@@ -226,26 +227,32 @@ ECDefineDebugChannel(ComaModelChannel);
       }
     }];
   }];
-
+  
   [self buildPropertyListsForClasses:classes];
   [self buildAllPropertiesListsForClasses:classes];
+  
+  // add the classInfo as a direct property - this allows templates to access this information explicitly, even in nested contexts where it might be masked
+  [classes enumerateKeysAndObjectsUsingBlock:^(id className, NSMutableDictionary* info, BOOL *stop) {
+    info[@"classInfo"] = [info copy];
+  }];
+  
 }
 
 - (void)buildPropertyListsForClasses:(NSMutableDictionary*)classes {
   // second pass through classes to process the attribute and relationship values
   [classes enumerateKeysAndObjectsUsingBlock:^(id className, NSMutableDictionary* info, BOOL *stop) {
-
+    
     // process attributes
     NSMutableDictionary* attributes = info[@"attributes"];
     [attributes enumerateKeysAndObjectsUsingBlock:^(id key, NSMutableDictionary* info, BOOL *stop) {
       [self preprocessProperty:info name:key className:className];
     }];
-
+    
     NSArray* sortedAttributes = [self sortByName:[attributes allValues] ];
     info[@"attributes"] = sortedAttributes;
-
+    
     NSMutableArray* properties = [NSMutableArray arrayWithArray:sortedAttributes];
-
+    
     // process relationships
     NSMutableDictionary* relationships = info[@"relationships"];
     if (relationships)
@@ -253,16 +260,16 @@ ECDefineDebugChannel(ComaModelChannel);
       [relationships enumerateKeysAndObjectsUsingBlock:^(id key, NSMutableDictionary* info, BOOL *stop) {
         [self preprocessProperty:info name:key className:className];
       }];
-
+      
       NSArray* sortedRelationships = [self sortByName:[relationships allValues]];
       info[@"relationships"] = sortedRelationships;
       [properties addObjectsFromArray:sortedRelationships];
     }
-
+    
     // combine attributes and relationships into a properties list
     NSArray* sortedProperties = [self sortByName:properties];
     info[@"properties"] = sortedProperties;
-
+    
     [self addFiltersToDictionary:info];
   }];
 }
@@ -273,7 +280,7 @@ ECDefineDebugChannel(ComaModelChannel);
     NSString* name2 = prop2[@"name"];
     return [name1 compare:name2];
   }];
-
+  
   return result;
 }
 
@@ -283,16 +290,16 @@ ECDefineDebugChannel(ComaModelChannel);
     NSMutableArray* allProperties = [NSMutableArray array];
     [self addItemsToArray:allProperties forClass:className key:@"properties"];
     info[@"allProperties"] = [self sortByName:allProperties];
-
+    
     NSMutableArray* allAttributes = [NSMutableArray array];
     [self addItemsToArray:allAttributes forClass:className key:@"attributes"];
     info[@"allAttributes"] = [self sortByName:allAttributes];
-
+    
     NSMutableArray* allRelationships = [NSMutableArray array];
     [self addItemsToArray:allRelationships forClass:className key:@"relationships"];
     info[@"allRelationships"] = [self sortByName:allRelationships];
   }];
-
+  
 }
 
 - (void)addItemsToArray:(NSMutableArray*)array forClass:(NSString*)className key:(NSString*)key {
@@ -303,7 +310,7 @@ ECDefineDebugChannel(ComaModelChannel);
     {
       [self addItemsToArray:array forClass:superclass key:key];
     }
-
+    
     [array addObjectsFromArray:info[key]];
   }
 }
@@ -317,7 +324,7 @@ ECDefineDebugChannel(ComaModelChannel);
   // add the name of the property to the info dictionary, so we can fetch it later
   info[@"name"] = name;
   info[@"class"] = className;
-
+  
   // look up the property type and try to obtain some type info for it
   NSString* type = info[@"type"];
   NSDictionary* typeInfo = [self infoForTypeNamed:type useDefault:YES];
@@ -326,23 +333,23 @@ ECDefineDebugChannel(ComaModelChannel);
     NSString* resolvedName = typeInfo[@"resolvedTypeName"];
     if (resolvedName)
       info[@"type"] = resolvedName;
-
+    
     // if the type is one of our classes, add it to the requires property
     if ([self.classes objectForKey:type])
     {
       info[@"requires"] = [NSString stringWithFormat:@"%@.h", type];
     }
-
+    
     // add the type and metatype entries to the property info
     // this allows templates to pick up and use these attributes directly
     info[@"typeInfo"] = typeInfo;
-
+    
     // merge in any type info keys that don't have values set in the property
     [typeInfo enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
       if (!info[key])
         info[key] = obj;
     }];
-
+    
   }
   else
   {
@@ -353,61 +360,61 @@ ECDefineDebugChannel(ComaModelChannel);
 
 - (void)addFiltersToDictionary:(NSMutableDictionary*)dictionary
 {
-    dictionary[@"initialCapital"] = [GRMustacheFilter filterWithBlock:^id(id value) {
-        NSString* string = [value description];
-        return [string stringWithInitialCapital];
-    }];
-
-    dictionary[@"mixedcaps"] = [GRMustacheFilter filterWithBlock:^id(id value) {
+  dictionary[@"initialCapital"] = [GRMustacheFilter filterWithBlock:^id(id value) {
+    NSString* string = [value description];
+    return [string stringWithInitialCapital];
+  }];
+  
+  dictionary[@"mixedcaps"] = [GRMustacheFilter filterWithBlock:^id(id value) {
     NSString* string = [value description];
     return [string mixedcaseStringFromMixedCaseWithInitialCapital];
   }];
-
+  
   dictionary[@"mixedcapsInitialCapital"] = [GRMustacheFilter filterWithBlock:^id(id value) {
     NSString* string = [value description];
     return [string mixedcaseStringInitialCapitalFromMixedCase];
   }];
-
+  
   dictionary[@"uppedcaseUnderscoreFromMixed"] = [GRMustacheFilter filterWithBlock:^id(id value) {
     NSString* string = [value description];
     return [string uppercaseUnderscoreStringFromMixedCase];
   }];
-
+  
   dictionary[@"lowercaseUnderscoreFromMixed"] = [GRMustacheFilter filterWithBlock:^id(id value) {
     NSString* string = [value description];
     return [string lowercaseUnderscoreStringFromMixedCase];
   }];
-
+  
   dictionary[@"mixedcapsInitialCapitalFromWords"] = [GRMustacheFilter filterWithBlock:^id(id value) {
     NSString* string = [value description];
     return [string mixedcaseStringInitialCapitalFromWords];
   }];
-
+  
   dictionary[@"mixedcapsFromWords"] = [GRMustacheFilter filterWithBlock:^id(id value) {
     NSString* string = [value description];
     return [string mixedcaseStringFromWords];
   }];
-
+  
   dictionary[@"lowercaseUnderscoreFromWords"] = [GRMustacheFilter filterWithBlock:^id(id value) {
     NSString* string = [value description];
     return [string lowercaseUnderscoreStringFromWords];
   }];
-
+  
   dictionary[@"uppercaseUnderscoreFromWords"] = [GRMustacheFilter filterWithBlock:^id(id value) {
     NSString* string = [value description];
     return [string uppercaseUnderscoreStringFromWords];
   }];
-
+  
   dictionary[@"wordsFromMixed"] = [GRMustacheFilter filterWithBlock:^id(id value) {
     NSString* string = [value description];
     return [string stringBySplittingMixedCaps];
   }];
-
+  
   dictionary[@"wordsFromMixedCapitalized"] = [GRMustacheFilter filterWithBlock:^id(id value) {
     NSString* string = [value description];
     return [[string stringBySplittingMixedCaps] capitalizedString];
   }];
-
+  
 }
 
 - (NSString*)superclassNameFromInfo:(NSDictionary*)info {
@@ -418,7 +425,7 @@ ECDefineDebugChannel(ComaModelChannel);
     if ([superclass isKindOfClass:[NSDictionary class]])
       superclass = superclass[@"class"];
   }
-
+  
   return superclass;
 }
 
@@ -435,7 +442,7 @@ ECDefineDebugChannel(ComaModelChannel);
   {
     result = self.types[name];
   }
-
+  
   // if this is a class we're generating, merge in the info from the class definition
   NSDictionary* classInfo = self.classes[name];
   if (classInfo)
@@ -451,7 +458,7 @@ ECDefineDebugChannel(ComaModelChannel);
       result = classInfo;
     }
   }
-
+  
   // if there's no entry for this class, use the default info
   if (!result && useDefault)
   {
@@ -460,8 +467,8 @@ ECDefineDebugChannel(ComaModelChannel);
       defaultInfo[@"originalTypeName"] = defaultInfo[@"resolvedTypeName"] = name;
     result = defaultInfo;
   }
-
-
+  
+  
   if (result)
   {
     id superclass = [self superclassNameFromInfo:result];
@@ -475,7 +482,7 @@ ECDefineDebugChannel(ComaModelChannel);
       result = merged;
     }
   }
-
+  
   return result;
 }
 
